8a55541bcff7ffc59597bab7d9599a41bb3917a1
Rio Martinez <rio@riomartinez.com>
2024-08-08 14:45:04 +0200

    chore(toolchain): load wit-bindgen from bootstrap

--- wit-bindgen.bootstrap.cmake
+++ wit-bindgen.bootstrap.cmake
@@ -1,3 +1,98 @@
+function(_wit_bindgen_find_root wit_bindgen_version out_wit_bindgen_root)
+    if (DEFINED ENV{WIT_BINDGEN_INSTALLATION_ROOT})
+        set(root "$ENV{WIT_BINDGEN_INSTALLATION_ROOT}")
+    elseif(WIN32)
+        set(root "$ENV{LOCALAPPDATA}/wit-bindgen/${wit_bindgen_version}/cache")
+    else()
+        set(root "$ENV{HOME}/.cache/wit-bindgen/${wit_bindgen_version}")
+    endif()
+
+    set(${out_wit_bindgen_root}
+        ${root}
+        PARENT_SCOPE
+    )
+endfunction()
+
+function(wit_bindgen_bootstrap)
+    cmake_parse_arguments(
+        PARSE_ARGV 0 "arg"
+        ""
+        "WIT_BINDGEN_TAG;WIT_BINDGEN_BINARY_OUTPUT"
+        ""
+    )
+    if (DEFINED arg_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR
+        "internal error: ${CMAKE_CURRENT_FUNCTION} passed extra args:"
+        "${arg_UNPARSED_ARGUMENTS}"
+        )
+    endif()
+
+    # Locate cache root
+    if(DEFINED CACHE{_WIT_BINDGEN_ROOT})
+        set(wit_bindgen_root $CACHE{_WIT_BINDGEN_ROOT})
+    else()
+        _wit_bindgen_find_root("${arg_WIT_BINDGEN_TAG}" wit_bindgen_root)
+    endif()
+    if (NOT EXISTS ${wit_bindgen_root})
+        message(STATUS "Creating wit-bindgen cache directory: ${wit_bindgen_root}")
+        file(MAKE_DIRECTORY ${wit_bindgen_root})
+    else()
+        message(STATUS "Cache directory for wit-bindgen exists already: ${wit_bindgen_root}")
+    endif()
+
+    # Detect host identifier
+    set(host_os "${CMAKE_HOST_SYSTEM_NAME}")
+    string(TOLOWER "${host_os}" host_os)
+    if ("${host_os}" STREQUAL "darwin")
+        set(host_os "macos")
+    endif()
+    set(host_architecture "${CMAKE_HOST_SYSTEM_PROCESSOR}")
+    string(TOLOWER "${host_architecture}" host_architecture)
+    if ("${host_architecture}" STREQUAL "arm64")
+        set(host_architecture "aarch64")
+    endif()
+    set(host_identifier "${host_architecture}-${host_os}")
+
+    # Get wit-bindgen bare version
+    string(REGEX MATCH "([0-9.])+" wit_bindgen_version ${arg_WIT_BINDGEN_TAG})
+
+    # Setup paths for downloaded content
+    set(wit_bindgen_tarball_name "wit-bindgen-${wit_bindgen_version}-${host_identifier}.tar.gz")
+    set(wit_bindgen_tarball_path "${wit_bindgen_root}/${wit_bindgen_tarball_name}")
+
+    # Setup remote urls for downloadable content
+    set(wit_bindgen_release_base_url "https://github.com/bytecodealliance/wit-bindgen/releases/download/${arg_WIT_BINDGEN_TAG}")
+    set(wit_bindgen_tarball_url "${wit_bindgen_release_base_url}/${wit_bindgen_tarball_name}")
+
+    # Download wit_bindgen binary
+    if (NOT EXISTS ${wit_bindgen_tarball_path})
+        message(STATUS "Downloading wit-bindgen binary from ${wit_bindgen_tarball_url}")
+        file(DOWNLOAD ${wit_bindgen_tarball_url} ${wit_bindgen_tarball_path} STATUS wit_bindgen_dl_status)
+        list(GET wit_bindgen_dl_status 0 wit_bindgen_dl_failed)
+        if (wit_bindgen_dl_failed)
+        file(REMOVE ${wit_bindgen_tarball_path})
+        message(FATAL_ERROR "Download for wit-bindgen binary failed.")
+        else()
+        message(STATUS "Successfully downloaded wit-bindgen binary to ${wit_bindgen_tarball_path}")
+        endif()
+    else()
+        message(STATUS "wit-bindgen binary has already been downloaded and cached.")
+    endif()
+
+    # Extract wit-bindgen sysroot to cache directory
+    message(STATUS "Extracting wit-bindgen binary to ${wit_bindgen_root}")
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E tar xzf ${wit_bindgen_tarball_path}
+        WORKING_DIRECTORY ${wit_bindgen_root}
+    )
+
+    # Set location of wit-bindgen
+    set(${arg_WIT_BINDGEN_BINARY_OUTPUT}
+        "${wit_bindgen_root}/wit-bindgen-${wit_bindgen_version}-${host_identifier}/wit-bindgen"
+        PARENT_SCOPE
+    )
+endfunction()
+
 # Usage:
 #
 # wit_bindgen(
@@ -28,18 +123,9 @@ function(wit_bindgen)
        "${arg_INTERFACE_FILE_INPUT}"
     )
 
-    # Ensure wit-bindgen is installed and in path
-    find_program(WIT_BINDGEN_EXECUTABLE "wit-bindgen")
-    if(NOT WIT_BINDGEN_EXECUTABLE)
-        message(FATAL_ERROR
-            "internal error: wit-bindgen is not installed."
-            "https://github.com/bytecodealliance/wit-bindgen/blob/main/README.md#cli-installation"
-        )
-    endif()
-
     # Generate bindings from WIT file
     execute_process(
-        COMMAND ${WIT_BINDGEN_EXECUTABLE} c --out-dir "${arg_BINDINGS_DIR_INPUT}" "${arg_INTERFACE_FILE_INPUT}"
+        COMMAND ${_wit_bindgen_binary} c --out-dir "${arg_BINDINGS_DIR_INPUT}" "${arg_INTERFACE_FILE_INPUT}"
         RESULT_VARIABLE wit_codegen_result
         OUTPUT_VARIABLE wit_codegen_output
     )
