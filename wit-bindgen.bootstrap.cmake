290fb65b5f302e750bb6bce01b66b5b45914f3b6
Rio Martinez <rio@riomartinez.com>
2025-02-13 09:34:30 +0100

    fix: properly parse wit-bindgen result

--- wit-bindgen.bootstrap.cmake
+++ wit-bindgen.bootstrap.cmake
@@ -135,6 +135,7 @@ function(wit_bindgen)
 
     # Scrape generated files from wit-bindgen
     string(REGEX MATCHALL "\"[^\"\\ ]+.(c|h|o)\"" wit_codegen_output_files "${wit_codegen_output}")
+    string(REGEX MATCHALL "[^\"\\ ]+" wit_codegen_output_files "${wit_codegen_output_files}")
 
     # Set output variable to list of generated files
     set(${arg_GENERATED_FILES_OUTPUT}
