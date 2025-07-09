a214d7a0d96baf3e85267fe2d6f9ecf565d9ba17
Rio Martinez <rio@riomartinez.com>
2025-07-09 14:24:24 +0200

    chore: remove stub for mmap, add mremap

--- libc-stubs/include/sys/mman.h
+++ libc-stubs/include/sys/mman.h
@@ -8,3 +8,8 @@
 void* mmap(void* addr, size_t len, int prot, int flags, int fildes, off_t off);
 int munmap(void* addr, size_t len);
 int msync(void* addr, size_t len, int flags);
+void* mremap(void* old_address,
+             size_t old_size,
+             size_t new_size,
+             int flags,
+             ...);
