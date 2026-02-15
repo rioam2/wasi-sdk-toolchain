89de6befccc7f35881f44b38c1dbed8d8198abaf
Rio Martinez <rio@riomartinez.com>
2026-02-15 14:45:00 +0100

    chore: add smoke test

---
+++ tests/smoke_test/main.cpp
@@ -0,0 +1,9 @@
+/**
+ * Basic smoke test to verify that the toolchain can compile and run a simple
+ * C++ program. This test does not use any special features and serves as a
+ * sanity check for the toolchain setup and runtime environment.
+ */
+
+int main() {
+  return 0;
+}
