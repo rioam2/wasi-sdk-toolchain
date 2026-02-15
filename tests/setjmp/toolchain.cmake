9847569044c65e2820f09310c232ad685b6425fb
Rio Martinez <rio@riomartinez.com>
2026-02-15 14:22:50 +0100

    chore: add setjmp test

--- tests/setjmp/toolchain.cmake
+++ tests/setjmp/toolchain.cmake
@@ -3,10 +3,10 @@ include("$ENV{WASI_SDK_TOOLCHAIN_FILE}")
 
 # Initialize a specific version of the WASI toolchain
 initialize_wasi_toolchain(
-  WIT_BINDGEN_TAG "v0.42.1"
-  WASMTIME_TAG "v33.0.0"
-  WASM_TOOLS_TAG "v1.234.0"
-  WASI_SDK_TAG "wasi-sdk-29"
+  WIT_BINDGEN_TAG "v0.53.1"
+  WASMTIME_TAG "v41.0.3"
+  WASM_TOOLS_TAG "v1.245.1"
+  WASI_SDK_TAG "wasi-sdk-30"
   TARGET_TRIPLET "wasm32-wasi"
   ENABLE_EXPERIMENTAL_SETJMP ON
 )
