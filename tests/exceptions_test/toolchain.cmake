017126b9b3fdf111565b5eeac07beefdbc3335d9
Rio Martinez <rio@riomartinez.com>
2026-02-15 16:09:51 +0100

    feat: add experimental exceptions support

---
+++ tests/exceptions_test/toolchain.cmake
@@ -0,0 +1,11 @@
+# Source toolchain file(s)
+include("$ENV{WASI_SDK_TOOLCHAIN_FILE}")
+
+# Initialize a specific version of the WASI toolchain
+initialize_wasi_toolchain(
+  WIT_BINDGEN_TAG "v0.53.1"
+  WASMTIME_TAG "v41.0.3"
+  WASM_TOOLS_TAG "v1.245.1"
+  WASI_SDK_TAG "wasi-sdk-30.0-cpp-exn"
+  TARGET_TRIPLET "wasm32-wasi"
+)
